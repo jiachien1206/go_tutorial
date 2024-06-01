57ecab647157d2effbd648e1dc941b11c0fb8dbe
jiachien1206 <nini19961206@gmail.com>
2024-06-02 00:52:54 +0800

    feat(hello2): handle empty string error

--- hello2/hello.go
+++ hello2/hello.go
@@ -4,11 +4,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-    message := greetings.Hello("Nini")
-		fmt.Println(message)
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
 }
